c026fa12c4afaa9775efa2e73aae38874c1ba75e
liuyudi11@vip.qq.com <liuyudi11@vip.qq.com>
2018-07-10 22:14:13 +0800

    增加模块

--- Module/FileObject.py
+++ Module/FileObject.py
@@ -5,15 +5,17 @@ import os
 
 def getFile(dicPath,fileExten=[]):
     if not os.path.isdir(dicPath):
-        raise IOError('dicPath is not dir')
+        error = dicPath+'dicPath is not dir'
+        raise IOError(error)
         return None
     dirList = [dicPath]
     fileList = []
-    for path in fileList:
-        dirList.extend([path+'/'+thisFile for thisFile in os.listdir(path) if os.path.isdir(path+'/'+thisFile)])
+    for path in dirList:
+        allFile = [path+'/'+thisFile for thisFile in os.listdir(path) if not thisFile.startswith('.')]
+        dirList.extend([thisFile for thisFile in allFile if os.path.isdir(thisFile)])
         if len(fileExten) == 0:
-            fileList.extend(os.listdir(path))
+            print(fileExten)
+            fileList.extend(allFile)
         else:
-            fileList.extend([path+'/'+thisFile for thisFile in os.listdir(path) if os.path.split(path+'/'+thisFile)[1][1:] in fileExten])
-    print(fileList)
-
+            fileList.extend([thisFile for thisFile in allFile if os.path.splitext(thisFile)[1][1:] in fileExten])
+    print('\n===========================================\n\n'+fileList)
