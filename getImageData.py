393c2282dbd37e123366ec0f2944b6070aef6459
liuyudi11@vip.qq.com <liuyudi11@vip.qq.com>
2018-07-13 15:46:09 +0800

    .gitignore

--- getImageData.py
+++ getImageData.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import sys
 import re
-import argparse
-import imp
 import Module
 
 def deleteAnnoationCode(codeString):
@@ -16,21 +13,20 @@ def deleteAnnoationCode(codeString):
         codeString = codeString.replace(string,'')
     return codeString
 
-imp.reload(sys)
 path = os.getcwd() + "/.." #文件夹目录
 os.chdir(path)
 imageList = Module.FileObject.getFile(path,['imageset'])
 codePath = path
 fileList = [codePath]
 jointString = []
-for file in fileList:
-    files = os.listdir(file)
+for filePath in fileList:
+    files = os.listdir(filePath)
     for thisFile in files:
         if 'thirdparty' == thisFile:
             continue
         if thisFile.endswith('.m') or thisFile.endswith('.mm') or thisFile.endswith('.xib') or thisFile.endswith('.storyboard') or thisFile.endswith('.xml') or thisFile.endswith('.plist'):
             try :
-                codeFile = open(file+'/'+thisFile,encoding='utf8')
+                codeFile = open(filePath+'/'+thisFile,encoding='utf8')
             except IOError as e:
                 print('tryError')
             else:
@@ -42,14 +38,14 @@ for file in fileList:
                     codeString = deleteAnnoationCode(codeString)
                     connectImage = re.compile('"[^"\n%:=]+%').findall(codeString)
                     jointString.extend(connectImage)
-                    imageList = [thisImage for thisImage in imageList if os.path.splitext(thisImage)[0] not in codeString]
+                    imageList = [thisImage for thisImage in imageList if ('"'+os.path.splitext(os.path.basename(thisImage))[0] +'"') not in codeString]
                     codeFile.close()
-    dirFileList = [file+'/'+thisFile for thisFile in files if os.path.isdir(file+'/'+thisFile) and not thisFile == 'Pods' and '.' not in thisFile]
+    dirFileList = [filePath+'/'+thisFile for thisFile in files if os.path.isdir(filePath+'/'+thisFile) and not thisFile == 'Pods' and '.' not in thisFile]
     fileList.extend(dirFileList)
 index = len(imageList) - 1
 while (index >= 0):
     thisImage = imageList[index]
-    thisImage = os.path.splitext(thisImage)[0]
+    thisImage = os.path.splitext(os.path.basename(thisImage))[0]
     for joint in jointString:
         string = joint[1:-1]
         if thisImage.startswith(string):
